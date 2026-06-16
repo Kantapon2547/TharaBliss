672485e7bb8b1277de6fe51400e1e15e3695fa8d
Kantapon Hemmadhun <kantapon.hemmadhun@gmail.com>
2026-06-16 15:42:36 +0700

    refactor: move configuration to environment variables to redeploy

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -24,9 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-()a%t7k43_i8m3wb++4fmqded18$d3jv@is_s#vym3dr%^ul_3"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
